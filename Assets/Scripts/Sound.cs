2ef77e37d1d5a661509b2db6b22c332e60492b72
kardxd <arturkardasevich123@gmail.com>
2023-02-05 14:47:25 +0200

    test beta

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    
+    //[SerializeField] Slider slidervolume;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //audiolistener.volumue = slidervolume.value;
+    }
+     private void Load()
+    {
+      //  PlayerPrefs.SetFloat("slidervolume", slidervolume.value);
+    }
+
+    private void Save()
+    {
+      //  PlayerPrefs.SetFloat("slidervolume", slidervolume.value);
+    }
+}
